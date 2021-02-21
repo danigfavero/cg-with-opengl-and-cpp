d3b209e6d560d1ced301301826a5de1c4c3eef6a
danigfavero <danifavero@usp.br>
2021-02-21 16:36:32 -0300

    Shaders and the Rendering Pipeline

--- main.cpp
+++ main.cpp
@@ -1,10 +1,124 @@
 #include <stdio.h>
+#include <string.h>
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
 // Window dimensions
 const GLint WIDTH = 800, HEIGHT = 600;
+
+GLuint VAO, VBO, shader;
+
+// Vertex Shader (should be in another file)
+static const char* vShader = "                                  \n\
+#version 330                                                    \n\
+                                                                \n\
+layout (location=0) in vec3 pos; // in:input                    \n\
+                                                                \n\
+void main() {                                                   \n\
+    // (* 0.4) translates the points, in x and y axis           \n\
+    gl_Position = vec4(0.4 * pos.x, 0.4 * pos.y, pos.z, 1.0);   \n\
+}";
+
+// Fragment Shader
+static const char* fShader = "                                  \n\
+#version 330                                                    \n\
+                                                                \n\
+out vec4 color;                                                 \n\
+                                                                \n\
+void main() {                                                   \n\
+    color = vec4(1.0, 0.0, 0.0, 1.0);                           \n\
+}";
+
+void CreateTriangle() {
+    // window axis:
+    //    __________
+    //   |     1    |
+    //   |-1       1|
+    //   |____-1____|
+
+    GLfloat vertices[] = {
+        -1.0f, -1.0f, 0.0f,
+        1.0, -1.0f, 0.0f,
+        0.0f, 1.0f, 0.0f
+    };
+
+    glGenVertexArrays(1, &VAO); // &VAO is the id
+    glBindVertexArray(VAO);
+
+    glGenBuffers(1, &VBO); // &VBO is the id
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0); // unbind
+
+    glBindVertexArray(0); // unbind
+}
+
+void AddShader(GLuint theProgram, const char* shaderCode, GLenum shaderType) {
+    GLuint theShader = glCreateShader(shaderType);
+
+    const GLchar* theCode[1];
+    theCode[0] = shaderCode;
+
+    GLint codeLength[1];
+    codeLength[0] = strlen(shaderCode);
+
+    glShaderSource(theShader, 1, theCode, codeLength);
+    glCompileShader(theShader);
+
+    // error logs, to avoid debugging
+    GLint result = 0;
+    GLchar eLog[1024] = { 0 };
+
+    glGetShaderiv(theShader, GL_COMPILE_STATUS, &result);
+    if (!result) {
+        glGetShaderInfoLog(theShader, sizeof(eLog), NULL, eLog);
+        printf("Error compiling the %d shader: '%s' \n", shaderType, eLog);
+        return;
+    }
+
+    // finally attaching shader to the program
+    glAttachShader(theProgram, theShader);
+}
+
+void CompileShaders() {
+    shader = glCreateProgram();
+
+    if (!shader) {
+        printf("Error creating shader program!\n");
+        return;
+    }
+
+    AddShader(shader, vShader, GL_VERTEX_SHADER);
+    AddShader(shader, fShader, GL_FRAGMENT_SHADER);
+
+    // error logs, to avoid debugging
+    GLint result = 0;
+    GLchar eLog[1024] = { 0 };
+
+    glLinkProgram(shader);
+    glGetProgramiv(shader, GL_LINK_STATUS, &result);
+    if (!result) {
+        glGetProgramInfoLog(shader, sizeof(eLog), NULL, eLog);
+        printf("Error linking program: '%s' \n", eLog);
+        return;
+    }
+
+    // validations
+    glValidateProgram(shader);
+    glGetProgramiv(shader, GL_VALIDATE_STATUS, &result);
+    if (!result) {
+        glGetProgramInfoLog(shader, sizeof(eLog), NULL, eLog);
+        printf("Error validating program: '%s'\n", eLog);
+        return;
+    }
+
+
+}
 
 int main() {
     
@@ -51,15 +165,28 @@ int main() {
     // Setup viewport size
     glViewport(0, 0, bufferWidth, bufferHeight);
 
+    // Now we're finally drawing!
+    CreateTriangle();
+    CompileShaders();
+
     // Loop until window closed
     while (!glfwWindowShouldClose(mainWindow)) {
         
         // Get + handle user input events
         glfwPollEvents();
 
-        // Clear window (red)
-        glClearColor(1.0f, 0.0f, 0.0f, 1.0f); // RGB -> [0,1], opacity (alpha)
+        // Clear window
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f); // RGB -> [0,1], opacity (alpha)
         glClear(GL_COLOR_BUFFER_BIT);
+
+        // Shaders and rendering pipeline
+        glUseProgram(shader);
+
+        glBindVertexArray(VAO); // bind
+        glDrawArrays(GL_TRIANGLES, 0, 3); // start from point 0, 3 points
+        glBindVertexArray(VAO); // unbind
+
+        glUseProgram(0); // close shader
 
         glfwSwapBuffers(mainWindow); // 2 scenes: drawing and user
     }
--- notes.md
+++ notes.md
@@ -1,8 +1,8 @@
 # Course Notes
 
-## Section 1: Introduction
+## **Section 1: Introduction**
 
-### GLEW, GLFW and SDL
+## GLEW, GLFW and SDL
 
 - **GLEW: OpenGL Extension Wrangler**
     - interface for OpenGL
@@ -40,3 +40,71 @@ g++ -std=c++11 main.cpp -o out -lGLEW -lGL -lGLU -lglfw3 -lX11 -lXxf86vm -lXrand
 ./out
 ```
 **TODO:** a makefile...
+
+---
+
+## **Section 2: Beginner**
+
+## Shaders and the rendering pipeline
+- Rendering pipeline: series of stages
+- 4 stages: programmable via shaders
+- shaders are piece of code written in CLSL (OpenGL Shading Language): in C
+
+### The rendering pipeline stages
+
+1. Vertex specification  
+- vertex $(x,y,z)$
+- primitive: simple shape defined by vertices
+- set up data of vertices for the primitives we want to render
+- done in the application itself
+- uses VAOs (vertex array objects, defines what data a vertex has) and VBOs (vertex buffer objects, defines the data itself)
+- attribute pointers define where and how shaders can access vertex data
+    1. create VAO/VBO
+    1. initiate draw
+
+1. Vertex shader (programmable)
+- handles vertices individually
+- not optional
+- can specify additional outputs
+- inputs consist of the vertex data itself
+
+1. Tessellation (programmable)
+- allows you to divide data in to smaller primitives
+- can be used to add higher levels of detail dynamically
+
+1. Geometry shade (programmable)
+- handles primitives
+- takes primitives then "emits" their vertices to create the given primitives
+- can create and modify primitives
+- can alter primitive type
+
+1. Vertex post-processing
+    1. Tranform Feedback
+    - result of vertex and geometry stages saved to buffers for later use
+    1. Clipping
+    - primitive that won't be visible are removed
+    - positions converted from "clip-space" to "window space"
+
+1. Primitive assembly
+- vertices converted in to a series of primitives
+- face culling: removal of primitives that can't be seen
+
+1. Rasterization
+- converts primitives to "fragments"
+- fragments are pieces of data for each pixel
+- fragment data will be interpolated based on its position relative to each vertex
+
+1. Fragment shader (programmable)
+- handles data for each fragment
+- optional but it's rare not to use it (exceptions are cases where only depth or stencil data is required)
+- most important output is the color of the pixel that the fragment covers
+
+1. Per-sample operations
+- series of tests run to see if the fragment should be drawn
+- most important test: DEPTH TEST (determines if something is in front of the point being drawn)
+- color blending: overlapping fragments - used to handle transparency
+- fragment data written to currently bound framebuffer
+- lastly, buffer swap
+
+### Shader programs
+- Created with at least a vertex shader and then activated before use
