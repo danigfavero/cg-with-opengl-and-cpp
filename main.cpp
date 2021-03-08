372c5155a54756f709149387d4852bf78888097f
danigfavero <danifavero@usp.br>
2021-03-08 16:21:10 -0300

    Projection

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@
 const GLint WIDTH = 800, HEIGHT = 600;
 const float toRadians = 3.14159265f / 180.0f;
 
-GLuint VAO, VBO, IBO, shader, uniformModel;
+GLuint VAO, VBO, IBO, shader, uniformModel, uniformProjection;
 
 // Uniform variables
 bool direction = true;
@@ -37,9 +37,10 @@ layout (location=0) in vec3 pos; // in:input                    \n\
 out vec4 vColor;                                                \n\
                                                                 \n\
 uniform mat4 model;                                             \n\
+uniform mat4 projection;                                        \n\
                                                                 \n\
 void main() {                                                   \n\
-    gl_Position = model * vec4(pos, 1.0);                       \n\
+    gl_Position = projection * model * vec4(pos, 1.0);          \n\
     vColor = vec4(clamp(pos, 0.0f, 1.0f), 1.0f);                \n\
 }";
 
@@ -150,6 +151,7 @@ void CompileShaders() {
     }
 
     uniformModel = glGetUniformLocation(shader, "model"); // from layout location=0
+    uniformProjection = glGetUniformLocation(shader, "projection");
 }
 
 int main() {
@@ -194,7 +196,7 @@ int main() {
         return 1;
     }
 
-    // a face of the pyramid should not appear when in the back
+    // Adds depth to 3D image
     glEnable(GL_DEPTH_TEST);
 
     // Setup viewport size
@@ -203,6 +205,10 @@ int main() {
     // Now we're finally drawing!
     CreateTriangle();
     CompileShaders();
+
+    // Projection
+    glm::mat4 projection(1.0f);
+    projection = glm::perspective(45.0f, (GLfloat)bufferWidth/(GLfloat)bufferHeight, 0.1f, 100.0f);
 
     // Loop until window closed
     while (!glfwWindowShouldClose(mainWindow)) {
@@ -246,12 +252,13 @@ int main() {
 
         // 4x4 identity matrix
         glm::mat4 model(1.0f);
+        model = glm::translate(model, glm::vec3(0.0f, 0.0f, -2.5f));
         model = glm::rotate(model, curAngle * toRadians, glm::vec3(0.0f, 1.0f, 0.0f)); 
-        // model = glm::translate(model, glm::vec3(triOffset, 0.0f, 0.0f));
         model = glm::scale(model, glm::vec3(0.4f, 0.4f, 1.0f));
 
         // 4x4 float values matrix
         glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+        glUniformMatrix4fv(uniformProjection, 1, GL_FALSE, glm::value_ptr(projection));
 
         glBindVertexArray(VAO); // bind
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, IBO);
